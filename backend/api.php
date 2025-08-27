da3542c35419dfec338ac4e8e72be0e2213b3ebd
Ankit Tyagi <atyagi@i-a-i.com>
2025-08-27 06:15:07 -0700

    adding 204 response and preflight OPTIONS param

--- backend/api.php
+++ backend/api.php
@@ -3,6 +3,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204); // Explicitly return 204 No Content
     exit;
 }
 
